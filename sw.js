988a2d555b8c0969f6d4bb38b42ee6e218e1365e
absotechhelp-source <absotechhelp@gmail.com>
2026-05-31 16:52:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 //  To trigger update on all devices: bump CACHE_VERSION below.
 // ================================================================
 
-const CACHE_VERSION = 'vla-annuity-v17';
+const CACHE_VERSION = 'vla-annuity-v18';
 
 const CORE_ASSETS = [
   './',
