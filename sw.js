9aad4993a9909d5c055d62ca37cf335146cd75b6
absotechhelp-source <absotechhelp@gmail.com>
2026-07-05 15:21:08 +0200

    Update CACHE_VERSION to vla-annuity-v66

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 //  To trigger update on all devices: bump CACHE_VERSION below.
 // ================================================================
 
-const CACHE_VERSION = 'vla-annuity-v65';
+const CACHE_VERSION = 'vla-annuity-v66';
 
 const CORE_ASSETS = [
   './',
