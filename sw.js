8163e70506b8ffbd8a33205ef80eb9f4bafee0e9
absotechhelp-source <absotechhelp@gmail.com>
2026-07-05 18:53:26 +0200

    Bump CACHE_VERSION to vla-annuity-v67
    Updated CACHE_VERSION to trigger service worker update.

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 //  To trigger update on all devices: bump CACHE_VERSION below.
 // ================================================================
 
-const CACHE_VERSION = 'vla-annuity-v66';
+const CACHE_VERSION = 'vla-annuity-v67';
 
 const CORE_ASSETS = [
   './',
