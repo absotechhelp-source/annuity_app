b6f904f222ff66c19397587b913ebaf3cc2a7252
absotechhelp-source <absotechhelp@gmail.com>
2026-07-31 11:14:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 //  To trigger update on all devices: bump CACHE_VERSION below.
 // ================================================================
 
-const CACHE_VERSION = 'vla-annuity-v73';
+const CACHE_VERSION = 'vla-annuity-v74';
 
 const CORE_ASSETS = [
   './',
